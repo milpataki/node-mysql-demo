6ce746bd6af5339f5cdf39bd83630fb291616ae1
Milan Pataki <milanpataki9@gmail.com>
2023-05-06 09:47:35 +0200

    generated modul service controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './typeorm/entities/user';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -14,7 +15,7 @@ import { User } from './typeorm/entities/user';
     database: "nestjs-tutorial",
     entities: [User],
     synchronize: true
-  })],
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
